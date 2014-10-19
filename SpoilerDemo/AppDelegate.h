86e16fcd16643c76880e3f6b9311004b692ddb47
Sergey Yuzepovich <yuzepovich@gmail.com>
2014-10-19 19:37:04 +0400

    Initial Commit

---
+++ SpoilerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpoilerDemo
+//
+//  Created by Sergey Yuzepovich on 19.10.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SpoilerDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SpoilerDemo
+//
+//  Created by Sergey Yuzepovich on 19.10.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
